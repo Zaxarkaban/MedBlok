1044930c46f27d587d1bdf2810f62eb83a00f55b
DebtOfTime <sasha_kulagin098311@mail.ru>
2025-04-20 23:34:32 +0300

    Рутинка с псевдобд

--- DocumentGenerator/DocumentGenerator/obj/Debug/net8.0/DocumentGenerator.AssemblyInfo.cs
+++ DocumentGenerator/DocumentGenerator/obj/Debug/net8.0/DocumentGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DocumentGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c54195b5d31bfa014977660f6756478e491364")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a704fb2f2de28c0d7f5a7e4ed04cf4d18ee0f55c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DocumentGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DocumentGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
